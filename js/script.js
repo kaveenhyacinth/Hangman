e29967639476c3c374df59c76ac404959501d352
kaveenhyacinth <alwiskaveen@gmail.com>
2020-08-28 11:04:58 +0530

    Modify UI

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const TeamOne = document.querySelector("#team-1");
+const TeamTwo = document.querySelector("#team-2");
+
+// window.onload = () => {
+
+//     var Team1 = prompt("Team 1");
+//     var Team2 = prompt("Team 2");
+
+//     TeamOne.innerText = Team1;
+//     TeamTwo.innerText = Team2;
+// }
