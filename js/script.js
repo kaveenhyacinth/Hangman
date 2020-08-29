4a7a7781dd8a1f5b01479b6668c785db40d78851
kaveenhyacinth <alwiskaveen@gmail.com>
2020-08-29 08:47:29 +0530

    Define winning logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const TeamOne = document.querySelector("#team-1");
 const TeamTwo = document.querySelector("#team-2");
+const scoreOne = document.querySelector("#score-1");
+const scoreTwo = document.querySelector("#score-2");
 const livesLeft = document.querySelector("#guesses");
 const dropzoneDiv = document.querySelector("#dropzone");
 const wrongDropZone = document.querySelector("#dropzone-wrong");
@@ -8,31 +10,27 @@ const blanksContainer = document.querySelector("#blanks");
 var isFirstTime = true;
 var isTeamOne = true;
 var lives = 5;
+var Score1 = 0;
+var Score2 =0;
 
 var wordToGuess;
 var numberOfBlanks;
+var guessRun;
+var Team1, Team2;
+
 
 /* get team name | get secret word to guess | generate blanks */
 window.onload = () => {
 
-    var Team1 = prompt("Enter Team 1 Name");
-    var Team2 = prompt("Enter Team 2 Name");
+    Team1 = prompt("Enter Team 1 Name");
+    Team2 = prompt("Enter Team 2 Name");
 
-    if(isTeamOne) {
-        wordToGuess = prompt(`Team ${Team1.toUpperCase()} please enter a secret word to guess`);
-        isTeamOne = !isTeamOne;
-    }
+    getSecretWord();
 
     TeamOne.innerText = Team1.toUpperCase();
     TeamTwo.innerText = Team2.toUpperCase();
     wordToGuess = wordToGuess.toUpperCase();
-
-    numberOfBlanks = countBlanks(wordToGuess);
-
-    for(let i=0; i < numberOfBlanks; i++) {
-        blanksContainer.innerHTML += `<div id='blank-${i}' class='blank'>__</div>`;
-    }
-
+    
     // debugging...
     console.log("onLoad is running");
     console.log(wordToGuess.toUpperCase());
@@ -40,12 +38,27 @@ window.onload = () => {
     console.log(countBlanks(wordToGuess));
 }
 
-/* Count the length of the entered secret word */
-const countBlanks = (word) => {
-    return word.length;
+/*=== Drag and drop start ===*/
+
+// grab the secret word
+const getSecretWord = () => {
+    if(isTeamOne) {
+        wordToGuess = prompt(`Team ${Team1.toUpperCase()} please enter a secret word to guess`);
+    } else {
+        wordToGuess = prompt(`Team ${Team2.toUpperCase()} please enter a secret word to guess`);
+    }
 }
 
-/*=== Drag and drop start ===*/
+// generate blanks according to secret word
+const generateBlanks = () => {
+    /* Count the length of the entered secret word */
+    numberOfBlanks = wordToGuess.length;
+    guessRun = wordToGuess.length;
+
+    for(let i=0; i < numberOfBlanks; i++) {
+        blanksContainer.innerHTML += `<div id='blank-${i}' class='blank'>__</div>`;
+    }
+}
 
 // get targeted item's id to transfer
 const onDragStart = event => {
@@ -97,7 +110,11 @@ const onDrop = event => {
 
     var indexArray = guessChecker(wordToGuess, guessLetter, id);
 
-    replaceBlanks(indexArray, guessLetter);
+    if(indexArray != -1) {
+        replaceBlanks(indexArray, guessLetter);
+    }
+
+    findSessionWinner();
 
     // debugging
     console.log("onDrop is running");
@@ -127,6 +144,8 @@ const guessChecker = (word, guess, id) => {
 
         lives -= 1;
         livesLeft.innerText = `0${lives}`;
+
+        return -1;
     }
 
     // debugging...
@@ -159,9 +178,55 @@ const replaceBlanks = (indexArray, guess) => {
         var resultToReplace = stringToReplace.replace("__", guess);
         replace.innerText = resultToReplace;
 
+        guessRun -= 1;
+
         // debugging...
         console.log("replaceBlanks is running");
         console.log(replace.innerText);
     }
 }
+
+// find the winner
+const findSessionWinner = () => {
+
+    var sessionLead = isTeamOne ? Team1 : Team2;
+    var sessionRunner = isTeamOne ? Team2 : Team1;
+
+    if(lives === 0) {
+        var winMsg = `🎉 ${sessionLead} has won 🎉`;
+        scoreUpdater(sessionLead);
+        alert(winMsg.toUpperCase());
+    } else if(guessRun === 0) {
+        var winMsg = `🎉 ${sessionRunner} has won 🎉`;
+        scoreUpdater(sessionRunner);
+        alert(winMsg.toUpperCase());
+    }
+
+    // debugging...
+    console.log("findSessionWinner is running");
+    console.log(`lead: ${ssessionLead} and runner: ${sessionRunner}`);
+}
+
+// Score updater
+const scoreUpdater = (Lead) => {
+    if(Lead == Team1) {
+        Score1++;
+        scoreOne.innerHTML = `0${Score1}`;
+    } else if(Lead == Team2) {
+        Score2++;
+        scoreTwo.innerHTML = `0${Score2}`;
+    }
+
+    // debugging...
+    console.log("scoreUpdater is running");
+    console.log(`Score1: ${Score1} and Score2: ${Score2}`);
+}
+
+// session handler
+const newSession = () => {
+    isTeamOne = !isTeamOne;
+    wordToGuess = null;
+    scoreOne = scoreTwo = 0;
+}
+
 
